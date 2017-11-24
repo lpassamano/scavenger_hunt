059f67bdc47612821b961afbc7f00bbd215381e4
lpassamano <leighayna.josele@gmail.com>
2017-11-24 15:36:36 -0500

    ran migrations and set up Postgres

--- db/migrate/20171124200453_create_found_items.rb
+++ db/migrate/20171124200453_create_found_items.rb
@@ -2,7 +2,7 @@ class CreateFoundItems < ActiveRecord::Migration[5.1]
   def change
     create_table :found_items do |t|
       t.boolean :found
-      t.iteger :team_id
+      t.integer :team_id
       t.integer :item_id
     end
   end
