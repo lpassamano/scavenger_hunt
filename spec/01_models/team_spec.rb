e128677a69747cd09c252424847feb902366d94f
lpassamano <leighayna.josele@gmail.com>
2017-11-26 13:50:59 -0500

    added Team model tests

---
+++ spec/01_models/team_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  before (:each) do
+    @team = Team.new
+    @user1 = User.create(name: Faker::Internet.user_name, email: Faker::Internet.email, password: "password123")
+    @user2 = User.create(name: Faker::Internet.user_name, email: Faker::Internet.email, password: "password123")
+    @user3 = User.create(name: Faker::Internet.user_name, email: Faker::Internet.email, password: "password123")
+    @hunt = Hunt.create(location: Location.first, user: @user1)
+    @item = Item.create(hunt: @hunt)
+  end
+
+  it 'has a hunt' do
+    @team.hunt = @hunt
+    @team.save
+
+    expect(@team.hunt).to eq(Hunt.last)
+  end
+
+  it 'has many participants' do
+    @team.participants << @user2
+    @team.participants << @user3
+
+    expect(@team.participants.count).to eq(2)
+  end
+
+  it 'has many found items' do
+    @team.found_items << FoundItem.new(item: @item)
+
+    expect(@team.found_items.count).to eq(1)
+  end
+end
