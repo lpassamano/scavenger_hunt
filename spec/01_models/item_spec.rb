e17094f45915feb784d60c8b7ab3213a594e6e2f
lpassamano <leighayna.josele@gmail.com>
2017-11-26 12:56:59 -0500

    added more Item model tests

--- spec/01_models/item_spec.rb
+++ spec/01_models/item_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before(:each) do
-    @item = Item.create(hunt: Hunt.first)
+    @item = Item.new(hunt: Hunt.create)
   end
 
   it 'has a hunt' do
-
+    expect(@item.hunt).to eq(Hunt.last)
   end
 
   it 'has a user' do
-    
+    expect(@item.user).to eq(@item.hunt.user)
   end
 end
