fed006d7610aa2db1f8e38836b2b53cd3c919ec2
lpassamano <leighayna.josele@gmail.com>
2017-11-26 11:37:42 -0500

    added User model test for hunts association

--- spec/01_models/user_spec.rb
+++ spec/01_models/user_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before (:each) do
     #create a user
+    @user = User.first
   end
 
   it 'has many hunts' do
+    @user.hunts << Hunt.find(1)
+    @user.hunts << Hunt.find(2)
+    @user.hunts << Hunt.find(3)
 
+    expect(@user.hunts.count).to eq(3)
   end
 
   it 'has many items' do
