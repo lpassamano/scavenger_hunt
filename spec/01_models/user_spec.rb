bd0cc6fb767c01c6b4f23f89d5dd646027502ea1
lpassamano <leighayna.josele@gmail.com>
2017-11-26 12:16:11 -0500

    added User model tests

--- spec/01_models/user_spec.rb
+++ spec/01_models/user_spec.rb
@@ -29,9 +29,15 @@ RSpec.describe User, type: :model do
   end
 
   it 'has a current team' do
+    @user.current_team = Team.create
+    expect(@user.current_team).to eq(Team.last)
   end
 
   it 'has a unique email and username' do
+    u = User.new(name: @user.name, email: "email@email.com", password: "password123")
+    expect(u.valid?).to eq(false)
 
+    u = User.new(name: "username123", email: @user.email, password: "password123")
+    expect(u.valid?).to eq(false)
   end
 end
