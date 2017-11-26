75b527940412785b7e95eb23e9ce7dc40c3b9148
lpassamano <leighayna.josele@gmail.com>
2017-11-26 10:54:24 -0500

    stubbed User model tests

--- spec/01_models/user_spec.rb
+++ spec/01_models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before (:each) do
+    #create a user
+  end
+
+  it 'has many hunts' do
+
+  end
+
+  it 'has many items' do
+
+  end
+
+  it 'has many teams' do
+
+  end
+
+  it 'has a current team' do
+
+  end
 end
