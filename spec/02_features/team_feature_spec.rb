e3e7b1e0629cef5f9bf28900019a48a5d725e48d
lpassamano <leighayna.josele@gmail.com>
2017-11-27 20:24:21 -0500

    added notes for team features spec

---
+++ spec/02_features/team_feature_spec.rb
@@ -0,0 +1,16 @@
+# show page
+  # when hunt is pending:
+    ## list all team members w/ link to their user pages
+    ## has join team button
+
+  # when hunt is active (for non-participants) or completed (for all users):
+    ## list all team members w/ link to their user pages
+    ## list all items in the hunt marking which ones were found
+
+  # when hunt is active for team participant:
+    ## page is a form where they can check off items that are found
+
+# new/edit team
+  # field for name
+  # option to invite users - autocomplete w/ registered users usernames?
+  # can be edited by any team participant 
