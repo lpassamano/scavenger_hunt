ca2919d5b8e52086f1e5ad1b59c780ac7550c3ac
lpassamano <leighayna.josele@gmail.com>
2017-11-27 20:18:59 -0500

    added notes for hunt features spec

---
+++ spec/02_features/hunt_feature_spec.rb
@@ -0,0 +1,20 @@
+# hunt show page
+  # while pending:
+    ## has edit button for its user
+    ## has delete button for its user
+    ## has join and create team button for all users
+    ## lists all items for the hunt
+    ## lists teams that have signed up already and the number of members in the team
+    ## team name is link to team show page
+
+  # while active:
+    ## has leaderboard listing teams by number of items found in descending order
+    ## link to team show page on the team name
+
+  # when completed:
+    ## shows final tally of all teams
+    ## link to team show page on the team name 
+
+# add hunt form
+
+# edit hunt form
