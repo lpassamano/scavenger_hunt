619654f62a2604cc0821c169f5bac6f7e6cac165
lpassamano <leighayna.josele@gmail.com>
2017-11-27 20:12:53 -0500

    added notes for home features spec

---
+++ spec/02_features/home_feature_spec.rb
@@ -0,0 +1,7 @@
+# home page lists all of users pending hunts
+# alerts user if they have a hunt in less than 48 hours
+# lists hunts near the user
+    # will have option for user to set home location if it is not already set
+    ### add location_id column to user
+# has link to view all hunts or search by location
+# has link to create new hunt 
