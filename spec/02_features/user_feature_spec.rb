e023025139a8e389cde63f1ad2763192633b3d25
lpassamano <leighayna.josele@gmail.com>
2017-11-27 20:36:16 -0500

    added notes for user features spec

---
+++ spec/02_features/user_feature_spec.rb
@@ -0,0 +1,11 @@
+# show
+  ## lists pending hunts and teams that user is participating in
+  ## lists home location
+  ## lists username
+  ## has pictures from fb or default image?
+  ## lists all hunts user has created, organized by start date descending (completed have different styling?)
+  ## if it is the current_user's page, gives them the option to edit their home location
+
+# edit
+  # /users/user-slug/location/edit
+  # only to update home location
