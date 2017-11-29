e2311068110ae895599be4c1ef2a81c378cb4db7
lpassamano <leighayna.josele@gmail.com>
2017-11-29 08:24:23 -0500

    switched logic of displaying pending hunts on home page

--- spec/02_features/home_feature_spec.rb
+++ spec/02_features/home_feature_spec.rb
@@ -9,22 +9,23 @@ describe 'Feature Test: Home', :type => :feature do
 
     context "logged in" do
       before(:each) do
-        @user = User.find(2)
+        @user = User.find(1)
         login_as(@user, scope: :user)
       end
 
       it "displays all of the current user's pending hunts with link to hunt's show page and their team name" do
         visit root_path
-        @user.hunts.pending.each do |hunt|
-          binding.pry
-          expect(page).to have_link(hunt.name, href: hunt_path(hunt), href: hunt_path(hunt))
-          expect(page).to have_link(@user.team(hunt).name, href: team_path(@user.team(hunt)))
+        @user.teams.each do |team|
+          if team.hunt.status == "pending"
+            expect(page).to have_link(team.hunt.name, href: hunt_path(team.hunt))
+            expect(page).to have_link(team.name, href: team_path(team))
+          end
         end
       end
 
       it "alerts the current user if they have a hunt starting in less than 48 hours" do
         visit root_path
-        #create method User#hunts.upcoming and use this for alert 
+        #create method User#hunts.upcoming and use this for alert
       end
     end
   end
