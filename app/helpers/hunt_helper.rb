7d332bdc29649a6f67facac35bdec9cfe77cf215
lpassamano <leighayna.josele@gmail.com>
2017-12-01 11:43:43 -0500

    added hunt helper methods

--- app/helpers/hunt_helper.rb
+++ app/helpers/hunt_helper.rb
@@ -1,2 +1,9 @@
 module HuntHelper
+  def li_for_hunt(hunt, method)
+    content_tag(:li, send(method, hunt), class: ("upcoming_hunt" if hunt.upcoming?))
+  end
+
+  def with_date(hunt)
+    link_to("#{hunt.name}", hunt_path(hunt)) + " | #{hunt.date}"
+  end
 end
