feb95efc2258fe716748f73e890a09001d8f13b8
lpassamano <leighayna.josele@gmail.com>
2017-12-01 09:30:44 -0500

    added Hunt feature spec for filtering index pg

--- spec/02_features/hunt_feature_spec.rb
+++ spec/02_features/hunt_feature_spec.rb
@@ -29,7 +29,11 @@ describe 'Feature Test: Hunts', :type => :feature do
       it 'has the ability to filter by location' do
         ## url = /hunts/location-slug
         location = Location.find_by(city: "New York")
-        visit hunts_path(location)
+        visit hunts_path
+        #need to have test select the city and state and click button!
+        find("#filter_hunts_city").find(:xpath, location.city).select_option
+        find("#filter_hunts_state").find(:xpath, location.state).select_option
+        click_button("Filter")
 
         Hunt.all_pending.each do |hunt|
           if hunt.location == location
