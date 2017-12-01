d7bd8e08c4bbd77b7e8182d384625ec080424996
lpassamano <leighayna.josele@gmail.com>
2017-12-01 11:53:28 -0500

    refactored and added hunt helper methods

--- app/helpers/hunt_helper.rb
+++ app/helpers/hunt_helper.rb
@@ -1,9 +1,13 @@
 module HuntHelper
-  def li_for_hunt(hunt, method)
-    content_tag(:li, send(method, hunt), class: ("upcoming_hunt" if hunt.upcoming?))
+  def li_for_hunt(hunt, *args, method)
+    content_tag(:li, send(method, hunt, *args), class: ("upcoming_hunt" if hunt.upcoming?))
   end
 
   def with_date(hunt)
     link_to("#{hunt.name}", hunt_path(hunt)) + " | #{hunt.date}"
+  end
+
+  def with_date_and_team(hunt, team)
+    with_date(hunt) + tag(:br) + link_to("#{team.name}", team_path(team))
   end
 end
