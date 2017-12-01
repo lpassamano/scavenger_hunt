390d3d23a236ddf632e6a0f27a6ad4dab87abd7f
lpassamano <leighayna.josele@gmail.com>
2017-12-01 12:29:00 -0500

    added helper method to display nearby hunts on welcome page

--- app/helpers/hunt_helper.rb
+++ app/helpers/hunt_helper.rb
@@ -10,4 +10,16 @@ module HuntHelper
   def with_date_and_team(hunt, team)
     with_date(hunt) + tag(:br) + link_to("#{team.name}", team_path(team))
   end
+
+  def display_nearby_hunts(user)
+    if !!user.location
+      html = []
+      Hunt.pending_in(user.location).each do |hunt|
+          html << li_for_hunt(hunt, :with_date)
+        end
+      content_tag(:ul, html.join.html_safe)
+    else
+      link_to("Add Your Location", edit_user_path(user))
+    end
+  end
 end
