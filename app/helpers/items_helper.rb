a38d2100b048eb817514404ff59d9be93a0489a7
lpassamano <leighayna.josele@gmail.com>
2017-12-04 22:59:21 -0500

    added helpers for displaying items

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,18 @@
 module ItemsHelper
+  def display_items_for(hunt)
+    html = []
+    hunt.items.each do |item|
+      html << li_for_item(item)
+    end
+    content_tag(:h3, "Items:") + content_tag(:ol, html.join.html_safe)
+  end
+
+  def li_for_item(item)
+    if item.hunt.pending? && current_user == item.hunt.owner
+      text = tag(:br) + link_to("Edit Item", edit_hunt_item_path(item.hunt, item)) + button_to("Remove Item", hunt_item_path(@hunt, item), :method => "delete", :class => "destroy")
+    else
+      text = ""
+    end
+    content_tag(:li, item.name + text)
+  end
 end
