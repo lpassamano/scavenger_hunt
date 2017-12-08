be90a76977f99563d5387bf7b1375da2a0a5b4ce
lpassamano <leighayna.josele@gmail.com>
2017-12-07 19:36:08 -0500

    added require_login to TeamsController

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,14 @@
 class TeamsController < ApplicationController
+  before_action :require_login
+  
   def show
     @team = Team.find(params[:id])
     @hunt = Hunt.find(params[:hunt_id])
+  end
+
+  private
+
+  def require_login
+    return redirect_to root_path unless current_user
   end
 end
