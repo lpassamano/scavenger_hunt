628c1d7b602f92df0ee07c7e8f8bac025b7c56d1
lpassamano <leighayna.josele@gmail.com>
2017-12-07 20:19:50 -0500

    added funtionality to join team button

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,24 @@
 class TeamsController < ApplicationController
   before_action :require_login
-  
+
   def show
     @team = Team.find(params[:id])
     @hunt = Hunt.find(params[:hunt_id])
   end
 
+  def update
+    @team = Team.find(params[:id])
+    @hunt = Hunt.find(params[:hunt_id])
+    new_participant = User.find(params[:team][:participant_id])
+    @team.participants << new_participant
+    redirect_to hunt_team_path(@hunt, @team)
+  end
+
   private
+
+  def team_params
+    params.require(:team).permit(:participant)
+  end
 
   def require_login
     return redirect_to root_path unless current_user
