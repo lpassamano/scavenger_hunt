f84071c2851300135907061000c2d57811a8bb61
lpassamano <leighayna.josele@gmail.com>
2017-12-10 16:23:58 -0500

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #make sure to define root route!!
   resources :hunts do
