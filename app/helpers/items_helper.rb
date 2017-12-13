ab973c49ae69994487b6fd97a7e9d84598ca1d6a
lpassamano <leighayna.josele@gmail.com>
2017-12-12 21:12:28 -0500

    refactored helper

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,7 +3,7 @@ module ItemsHelper
     if hunt.active? || hunt.completed? || (hunt.pending? && current_user == hunt.owner)
       html = capture do
         hunt.items.each do |item|
-          concat content_tag(:li, "#{item.name}")
+          concat content_tag(:li, item.name)
         end
       end
       content_tag(:h3, "Items:") + content_tag(:ol, html)
