aeaed5897e73aec74fd734ad25b3b857bb9b6d59
lpassamano <leighayna.josele@gmail.com>
2017-12-13 19:42:37 -0500

    fixed bug so it redirects after sign up to add name and location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || !!current_user.name ? root_path : edit_user_path(current_user)
+     current_user.name.nil? ? edit_user_profile_path(current_user) : root_path
+  end
+
+  def after_sign_up_path_for(resource)
+     current_user.name.nil? ? edit_user_profile_path(current_user) : root_path
   end
 
   def require_login
--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -1,9 +1,7 @@
 class Users::CallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # create User.from_omniauth(auth)
     @user = User.from_omniauth(auth)
     sign_in_and_redirect @user
-    #render 'welcome/index'
   end
 
   def auth
