b2bbfddbaf4ee69c2903bdce3db65a0ff19f99b6
Leigh Passamano <leighayna.josele@gmail.com>
2019-03-22 14:11:40 -0400

    Update Item spec to factory and remove unnecessary tests

--- spec/01_models/item_spec.rb
+++ spec/01_models/item_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe Item, type: :model do
     expect(item).to_not eq(item2)
   end
 
-  it 'upon instantiation creates found_items for each team in its hunt' do
-    team = @hunt.teams.first
+  it 'create a found_item for each team in its hunt' do
+    hunt = create :hunt
+    team = create :team, hunt: hunt
     num_items = team.items.count
 
-    @hunt.items.build(name: "New Item")
-    @hunt.save
+    hunt.items.build(name: "New Item")
+    hunt.save
     expect(team.items.count).to eq(num_items + 1)
   end
 end
