ed24f7fb37db8dfc1263a723ee13d5a57224cc4f
Leigh Passamano <leighayna.josele@gmail.com>
2019-03-22 14:31:07 -0400

    Update to use factory and reduce number of tests

--- spec/01_models/team_spec.rb
+++ spec/01_models/team_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Team, type: :model do
 
     expect(team).to_not eq(team2)
   end
+
+  it 'will generate a name if one is not given at initialization' do
+    team = create :team
+    expect(team.name).to eq("Team #{team.id}")
+  end
+
+  it 'creates a found_item for each item in its hunt' do
+    hunt = create :hunt
+    create :item, hunt: hunt
+
+    expect{
+      hunt.teams.create(name: "A Team")
+    }.to change(FoundItem, :count).by(1)
+  end
 end
