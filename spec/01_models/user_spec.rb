bd3ae5be5645d620de5fc25b319d0107c16fbd8f
Leigh Passamano <leighayna.josele@gmail.com>
2019-03-22 14:39:09 -0400

    Use factories and get rid of unneeded tests

--- spec/01_models/user_spec.rb
+++ spec/01_models/user_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe User, type: :model do
 
     expect(user).to_not eq(user2)
   end
+
+  it 'has a unique email' do
+    create :user, email: 'email@example.com'
+    user2 = build :user, email: 'email@example.com'
+    expect(user2).to_not be_valid
+  end
+
+  it 'has a unique username' do
+    create :user, name: 'leigh_p'
+    user2 = build :user, name: 'leigh_p'
+    expect(user2).to_not be_valid
+  end
 end
